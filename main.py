44a7d3e3dcece0826f94c7a4c8859a90e6d66e67
Ruan <ruan.fp@gmail.com>
2022-10-18 10:26:14 -0300

    added barchart in results

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 header = [f'Q{n}' for n in range(15)] + ['type']
 results_df = pd.read_csv('data/results.csv', sep=";", names=header)
@@ -45,10 +47,14 @@ for i, row in questions_df.iterrows():
 # x = st.slider('x', min_value=-2, max_value=2, step=1, value=0)  # 👈 this is a widget
 # st.write(x, 'squared is', x * x)
 
+
+
 if st.button('Run'):
     answer = [mapping.get(x, 0) for x in question_sliders]
     pred = neigh.predict_proba([answer])[0]
-    order = np.argsort(-pred)
-    for p, c in zip(pred[order], neigh.classes_[order]):
-        if p > 0:
-            st.text(f"({p:.0%}) {c}")
+    order = np.argsort(pred)
+  
+    fig, ax = plt.subplots()
+    ax.barh(neigh.classes_[order], pred[order]*100)
+    ax.xaxis.set_major_formatter(mtick.PercentFormatter())
+    st.pyplot(fig)
